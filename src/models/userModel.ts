ecad8ae52bd1486019cda6028ac567979bd125d3
RafaelCantoVazquez <rafacantovaz@gmail.com>
2022-02-25 21:51:11 -0600

    feat: add name field to user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
