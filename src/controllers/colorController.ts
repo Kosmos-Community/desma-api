45d5c260f386e7c2d75e8828f4b1280ddefc07d4
RafaelCantoVazquez <rafacantovaz@gmail.com>
2022-02-28 18:50:05 -0600

    fix: change http status code in palette controller methods responses

--- src/controllers/colorController.ts
+++ src/controllers/colorController.ts
@@ -1,23 +1,30 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { insertManyColors, deleteManyColors } from '../utils/colorFunctions';
 
+export const createColors = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const data = req.body;
+    const colors = await insertManyColors(data);
+    res.status(200).json(colors);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createColors = async (req: Request, res: Response) => {
-  const data = req.body;
-  const colors = await insertManyColors(data)
-    .catch((err) => {
-      throw new Error(err.message)
-    })
-  res.status(200).json(colors)
-}
-
-export const deleteColors = async (req: Request, res: Response) => {
-  const { ids } = req.body
-  const deletedColors = await deleteManyColors(ids)
-    .catch((err) => {
-      throw new Error(err.message)
-    })
-  res.status(200).json(deletedColors)
-}
-
-
+export const deleteColors = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { ids } = req.body;
+    const deletedColors = await deleteManyColors(ids);
+    res.status(200).json(deletedColors);
+  } catch (error) {
+    next(error);
+  }
+};
