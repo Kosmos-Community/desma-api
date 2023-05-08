88ef00c1cecc26e0633dc7378180cd0f5245c4d6
RafaelCantoVazquez <rafacantovaz@gmail.com>
2023-05-08 00:33:28 -0600

    Add tests for user register and login with Google

--- __tests__/routes/user.routes.test.ts
+++ __tests__/routes/user.routes.test.ts
@@ -73,4 +73,74 @@ describe('Testing the user routes module', () => {
     );
     expect(res.status).toBe(400);
   });
+
+  test('Calling register route with correct body and Google ID should return a token', async () => {
+    const userData = {
+      name: 'User',
+      email: `test.${Math.random().toString(36).slice(2)}@test.com`,
+      password: 'Password$',
+      googleId: Math.random().toString(10).substring(2, 23),
+    };
+    const res = await request(app).post('/api/users').send(userData);
+    expect(res.body.token).toBeDefined();
+    expect(res.status).toBe(201);
+  });
+
+  test('Calling login route with correct data and Google ID should return a token', async () => {
+    const googleId = Math.random().toString(10).substring(2, 23);
+    const userRegisterData = {
+      name: 'User',
+      email: `test.${Math.random().toString(36).slice(2)}@test.com`,
+      password: 'Password$',
+      googleId,
+    };
+    await request(app).post('/api/users').send(userRegisterData);
+
+    const userData = {
+      googleId,
+    };
+    const res = await request(app).post('/api/users/login').send(userData);
+    expect(res.body.token).toBeDefined();
+    expect(res.status).toBe(200);
+  });
+
+  test('Calling login route with invalid Google ID should return an error message', async () => {
+    const googleId = Math.random().toString(10).substring(2, 23);
+    const userData = {
+      googleId,
+    };
+    
+    const res = await request(app).post('/api/users/login').send(userData);
+    expect(res.body.message).toBe('User with associated Google ID not found');
+    expect(res.status).toBe(400);
+  });
+
+  test('Calling login route with correct data and without a Google ID should return a token', async () => {
+    const userEmail = `test.${Math.random().toString(36).slice(2)}@test.com`;
+    const userRegisterData = {
+      name: 'User',
+      email: userEmail,
+      password: 'Password$',
+    };
+    await request(app).post('/api/users').send(userRegisterData);
+
+    const userData = {
+      email: userEmail,
+      password: 'Password$',
+    };
+    const res = await request(app).post('/api/users/login').send(userData);
+    expect(res.body.token).toBeDefined();
+    expect(res.status).toBe(200);
+  });
+
+  test('Calling login route with invalid email and password should return an error message', async () => {
+    const userData = {
+      email: 'user@test',
+      password: 'password',
+    };
+    
+    const res = await request(app).post('/api/users/login').send(userData);
+    expect(res.body.message).toBe('Invalid email or password');
+    expect(res.status).toBe(400);
+  });
 });
